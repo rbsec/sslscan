382bbf72e76ffda615430e634ffa2c9fca84e054
rbsec <robin@rbsec.net>
2014-04-10 13:17:58 +0100

    Better heartbleed debugging

--- sslscan.c
+++ sslscan.c
@@ -1035,8 +1035,9 @@ int testHeartbleed(struct sslCheckOptions *options, const SSL_METHOD *sslMethod)
             char typ = hbbuf[0];
 
             // Combine 2 bytes to get payload length
+            uint16_t ver = hbbuf[2] | hbbuf[1] << 8;
             uint16_t ln = hbbuf[4] | hbbuf[3] << 8;
-//            printf("%hhX %hhX %hhX %hhX %hhX - %d\n", hbbuf[0], hbbuf[1], hbbuf[2], hbbuf[3], hbbuf[4], ln);
+//            printf("%hhX %hhX %hhX %hhX %hhX - %d %d %d\n", hbbuf[0], hbbuf[1], hbbuf[2], hbbuf[3], hbbuf[4], typ, ver, ln);
 
             memset(hbbuf, 0, sizeof(hbbuf));
             readResult = read(socketDescriptor, hbbuf, ln );
