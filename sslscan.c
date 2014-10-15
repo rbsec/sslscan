65ab746ddd32a01918c1ca2d1abbb49b9c065836
rbsec <robin@rbsec.net>
2014-10-15 15:21:57 +0100

    Added --rdp option for scanning RDP servers - credit skettler

--- sslscan.c
+++ sslscan.c
@@ -144,6 +144,7 @@ struct sslCheckOptions
     int targets;
     int sslbugs;
     int http;
+    int rdp;
     int verbose;
     int ipv4;
     int ipv6;
@@ -482,6 +483,38 @@ int tcpConnect(struct sslCheckOptions *options)
         printf_verbose("Server reported: %s\n", buffer);
     }
 
+    // Setup an RDP socket with preamble
+    // Borrowed from https://labs.portcullis.co.uk/tools/ssl-cipher-suite-enum/
+    if (options->rdp == true && tlsStarted == false)
+    {
+        unsigned char buffer[32768];
+        size_t readlen;
+
+        tlsStarted = 1;
+
+        // Send RDP preamble
+        send(socketDescriptor, "\x03\x00\x00\x13\x0e\xe0\x00\x00\x00\x00\x00\x01\x00\x08\x00\x03\x00\x00\x00", 19, 0);
+
+        // Read reply header
+        if (4 != recv(socketDescriptor, buffer, 4, 0)) {
+            printf_error("%s    ERROR: unexpected EOF reading from %s:%d%s\n", COL_RED, options->host, options->port, RESET);
+            return 0;
+        }
+
+        // Calculate remaining bytes (and check for overflows)
+        readlen = ((buffer[2] & 0x7f) << 8) + buffer[3] - 4;
+        if (readlen > sizeof(buffer)) {
+            printf_error("%s    ERROR: unexpected data from %s:%d%s\n", COL_RED, options->host, options->port, RESET);
+            return 0;
+
+        }
+
+        // Read reply data
+        if (readlen != recv(socketDescriptor, buffer, readlen, 0)) {
+            printf_error("%s    ERROR: unexpected EOF reading from %s:%d%s\n", COL_RED, options->host, options->port, RESET);
+            return 0;
+        }
+    }
     // Return
     return socketDescriptor;
 }
@@ -2548,6 +2581,10 @@ int main(int argc, char *argv[])
         else if (strcmp("--http", argv[argLoop]) == 0)
             options.http = 1;
 
+        // RDP Preamble...
+        else if (strcmp("--rdp", argv[argLoop]) == 0)
+            options.rdp = 1;
+
         // IPv4 only
         else if (strcmp("--ipv4", argv[argLoop]) == 0)
             options.ipv6 = false;
@@ -2616,6 +2653,8 @@ int main(int argc, char *argv[])
                     options.port = 25;
                 if (options.starttls_xmpp)
                     options.port = 5222;
+                if (options.rdp)
+                    options.port = 3389;
                 if (options.port == 0)
                     options.port = 443;
             }
@@ -2694,6 +2733,7 @@ int main(int argc, char *argv[])
             printf("  %s--starttls-smtp%s      STARTTLS setup for SMTP\n", COL_GREEN, RESET);
             printf("  %s--starttls-xmpp%s      STARTTLS setup for XMPP\n", COL_GREEN, RESET);
             printf("  %s--http%s               Test a HTTP connection.\n", COL_GREEN, RESET);
+            printf("  %s--rdp%s                Send RDP preamble before starting scan.\n", COL_GREEN, RESET);
             printf("  %s--bugs%s               Enable SSL implementation bug work-arounds\n", COL_GREEN, RESET);
             printf("  %s--xml=<file>%s         Output results to an XML file.\n", COL_GREEN, RESET);
             printf("  %s--version%s            Display the program version.\n", COL_GREEN, RESET);
