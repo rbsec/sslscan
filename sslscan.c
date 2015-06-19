9f030a501c3fc16585d26b13e3fa6ffe0f2e704b
shellster <shellster@example.com>
2015-06-19 07:26:00 -0700

    Added back --no-colour bug fix.

--- sslscan.c
+++ sslscan.c
@@ -2930,6 +2930,7 @@ int main(int argc, char *argv[])
             COL_BLUE = "";
             COL_GREEN = "";
             COL_PURPLE = "";
+	    COL_RED_BG = "";
         }
 
         // Client Certificates
